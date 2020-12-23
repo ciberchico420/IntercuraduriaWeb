82686d94a6dcefc91e235b0c9d0a3c0b445d773e
Casiel Didriksson <casield@gmail.com>
2020-12-23 17:47:29 -0600

    uploading build

--- src/components/home/HomeManager.js
+++ src/components/home/HomeManager.js
@@ -9,7 +9,7 @@ function HomeManager(props) {
     return (<div className="">
         
         <ButtonCircle className="cursosButton" empty={true} section="cursos" buttonText="CURSOS" size={30} top={30} left={3}><PageFromCategory category="2"></PageFromCategory></ButtonCircle>
-       <ButtonCircle className="nosotrasButton" empty={false} section="nosotras" buttonText="NOSOTRAS" size={10} top={10} left={80}><PageFromCategory category="5"></PageFromCategory></ButtonCircle>
+       <ButtonCircle className="nosotrasButton" empty={false} section="nosotras" buttonText="NOSOTRAS" size={10} top={10} left={80}><PageFromCategory category="2"></PageFromCategory></ButtonCircle>
         
       
     </div>)
